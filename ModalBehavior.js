5c1b6df82b25904ba16581b30ddf33998055eeba
Feek <fpm5022@gmail.com>
2016-05-18 17:52:53 -0400

    modal behavior

---
+++ ModalBehavior.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Feek on 5/18/16.
+ */
+/**
+ * Created by Feek on 3/15/16.
+ */
+define([
+	'marionette',
+	'../util/Vent'
+], function (
+	Mn,
+	Vent
+) {
+	var Modal = Mn.Behavior.extend({
+		/**
+		 * This behavior handles the behavior for a modal
+		 *
+		 * VIEW REQUIRES:
+		 * - template with .close (close button)
+		 * - template with .background (background div covering page)
+		 * - Vent with rootview listening to hide this
+		 *
+		 * @param options
+		 */
+		initialize: function(options) {
+		},
+
+		events: {
+			'click @ui.close' : 'close',
+			'click @ui.cancel' : 'close',
+			'click @ui.background' : 'close'
+		},
+
+		ui: {
+			close : '.close',
+			background: '.background',
+			cancel: '.cancel'
+		},
+
+		close: function() {
+			Vent.trigger('modal:close');
+		}
+	});
+
+	return Modal;
+});
